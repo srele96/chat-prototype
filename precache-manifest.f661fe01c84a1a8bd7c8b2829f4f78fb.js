5fdf4bc3b238605df71c24604affb45f4b6485a4
srele96 <srele96@gmail.com>
2020-10-19 22:10:28 +0200

    Updates

---
+++ precache-manifest.f661fe01c84a1a8bd7c8b2829f4f78fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22861aecfb8cafeaf9647f290568a0b8",
+    "url": "/chat-prototype/index.html"
+  },
+  {
+    "revision": "c914f7329aa8b4fb2d4e",
+    "url": "/chat-prototype/static/css/main.afd3280c.chunk.css"
+  },
+  {
+    "revision": "9bc284d400464bba036a",
+    "url": "/chat-prototype/static/js/2.e1e4f371.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chat-prototype/static/js/2.e1e4f371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c914f7329aa8b4fb2d4e",
+    "url": "/chat-prototype/static/js/main.49fb1cd4.chunk.js"
+  },
+  {
+    "revision": "ea9a4e38ec02e03b40d2",
+    "url": "/chat-prototype/static/js/runtime-main.570b9d75.js"
+  }
+]);
