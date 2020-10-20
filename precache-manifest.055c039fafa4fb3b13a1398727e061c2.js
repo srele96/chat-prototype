81daf73dcb4842059727b3a3b5f37cfd87c93da2
Srecko <srele96@gmail.com>
2020-10-20 20:37:07 +0200

    Updates

---
+++ precache-manifest.055c039fafa4fb3b13a1398727e061c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6498c3c6f69fb5f146baa72127b0fe5a",
+    "url": "/chat-prototype/index.html"
+  },
+  {
+    "revision": "e9687a344aa2760d9d57",
+    "url": "/chat-prototype/static/css/main.6039d232.chunk.css"
+  },
+  {
+    "revision": "d0aae42f0110df808575",
+    "url": "/chat-prototype/static/js/2.e1025d4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chat-prototype/static/js/2.e1025d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9687a344aa2760d9d57",
+    "url": "/chat-prototype/static/js/main.a3979092.chunk.js"
+  },
+  {
+    "revision": "ea9a4e38ec02e03b40d2",
+    "url": "/chat-prototype/static/js/runtime-main.570b9d75.js"
+  }
+]);
